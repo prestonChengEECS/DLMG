57ad042e82a28b7d390c0342cf3b6e8ba461341b
preston <96895234+itIsTimeToCode@users.noreply.github.com>
2026-07-09 21:50:57 -0700

    dont fade

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections; //necessary for IEnumerator
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public Vector3 checkpointPosition;
     public string checkpointScene;
+    public bool gameCompleted;
     void Awake()
     {
         if (instance == null)
@@ -31,6 +33,29 @@ public class GameManager : MonoBehaviour
         GameObject player = GameObject.FindWithTag("Player");
         if (player != null) {
             player.transform.position = checkpointPosition;
+        }
+    }
+
+    //we want to change DONT in the title to please
+    public void changeTitleScreen() {
+        GameObject dont = GameObject.FindWithTag("Dont");
+
+
+        if (dont != null) {
+            TextMeshProUGUI dontText = dont.GetComponent<TextMeshProUGUI>();
+
+            if (dontText != null) {
+                dontText.CrossFadeAlpha(0f, 2f, false);
+            }
+        }
+
+        
+    }
+
+    private void Update()
+    {
+        if (gameCompleted) {
+            changeTitleScreen();
         }
     }
 }
