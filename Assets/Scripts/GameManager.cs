db3ba3c5c39047ffce6526ad38738349f11b94ed
preston <96895234+itIsTimeToCode@users.noreply.github.com>
2026-07-11 12:55:17 -0700

    title change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections; //necessary for IEnumerator
 using TMPro;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,13 @@ public class GameManager : MonoBehaviour
     public Vector3 checkpointPosition;
     public string checkpointScene;
     public bool gameCompleted;
+
+    private bool hasEndTriggerFaded = false;
+
+    public float waitTime = 3f;
+
+    public float justFadeInTime = 3.0f;
+    public float dontFadeOutTime = 3.0f;
     void Awake()
     {
         if (instance == null)
@@ -22,10 +31,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        GameObject just = GameObject.FindWithTag("Just");
+        TextMeshProUGUI justText = just.GetComponent<TextMeshProUGUI>();
+
+        if (justText != null) {
+            justText.canvasRenderer.SetAlpha(0f);
+        }
+    }
+
     public void SetCheckpoint(Vector3 position)
     {
 
-        checkpointPosition = position; 
+        checkpointPosition = position;
         checkpointScene = SceneManager.GetActiveScene().name;
     }
 
@@ -36,26 +55,39 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //we want to change DONT in the title to please
-    public void changeTitleScreen() {
-        GameObject dont = GameObject.FindWithTag("Dont");
 
+    //we want to change DONT in the title to please
+    public async void changeTitleScreen() {
+
+        GameObject dont = GameObject.FindWithTag("Dont");
+        GameObject just = GameObject.FindWithTag("Just");
 
         if (dont != null) {
-            TextMeshProUGUI dontText = dont.GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI dontText = dont.GetComponent<TextMeshProUGUI>(); //gets the text component of the "Dont" gameObject
 
             if (dontText != null) {
-                dontText.CrossFadeAlpha(0f, 2f, false);
+                dontText.CrossFadeAlpha(0f, dontFadeOutTime, false);
             }
         }
 
-        
+        await Task.Delay((int)(waitTime * 1000));
+
+        if (just != null) {
+            TextMeshProUGUI justText = just.GetComponent<TextMeshProUGUI>();
+
+            if (justText != null) { 
+                justText.CrossFadeAlpha(1f, justFadeInTime, false);
+            }
+
+        }
     }
 
     private void Update()
     {
-        if (gameCompleted) {
+        if (gameCompleted && !hasEndTriggerFaded) {
+            hasEndTriggerFaded = true;
             changeTitleScreen();
         }
     }
+
 }
