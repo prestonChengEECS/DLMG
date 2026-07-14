7f99e8c57e0cbf381be717eb0ab3e308e16e115c
preston <96895234+itIsTimeToCode@users.noreply.github.com>
2026-07-13 21:47:50 -0700

    added lighters

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,8 +16,10 @@ public class GameManager : MonoBehaviour
 
     public float waitTime = 3f;
 
-    public float justFadeInTime = 3.0f;
+    public float justFadeInTime = 5.0f;
     public float dontFadeOutTime = 3.0f;
+
+    public Sprite lighterSpadeOpen;
     void Awake()
     {
         if (instance == null)
@@ -56,38 +58,19 @@ public class GameManager : MonoBehaviour
     }
 
 
-    //we want to change DONT in the title to please
-    public async void changeTitleScreen() {
-
-        GameObject dont = GameObject.FindWithTag("Dont");
-        GameObject just = GameObject.FindWithTag("Just");
-
-        if (dont != null) {
-            TextMeshProUGUI dontText = dont.GetComponent<TextMeshProUGUI>(); //gets the text component of the "Dont" gameObject
-
-            if (dontText != null) {
-                dontText.CrossFadeAlpha(0f, dontFadeOutTime, false);
-            }
-        }
-
-        await Task.Delay((int)(waitTime * 1000));
-
-        if (just != null) {
-            TextMeshProUGUI justText = just.GetComponent<TextMeshProUGUI>();
-
-            if (justText != null) { 
-                justText.CrossFadeAlpha(1f, justFadeInTime, false);
-            }
-
-        }
-    }
 
     private void Update()
     {
         if (gameCompleted && !hasEndTriggerFaded) {
             hasEndTriggerFaded = true;
-            changeTitleScreen();
         }
+    }
+
+    //we want to change DONT in the title to JUST instead. this helps do that.
+    //also changes the lighter from a closed position to an open position.
+    private IEnumerator changeTitleScreen() {
+        GameObject dont = GameObject.FindWithTag("Dont");
+        yield return new WaitForSeconds(2.0f);
     }
 
 }
